3799b5764e63257d448cd044189da1fda73d65ea
hershyheilpern <hershyheilpern@gmail.com>
2022-03-08 13:18:07 -0500

    add obj prefix

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ vue_component = {
            
         }
         obj.path = path.split(obj.name)[0]
+        obj.prefix = obj.name.slice(0,-4)
         fs.readFile(obj.path+obj.name,(err,file)=>{
             if(err){
                 cb(err)
@@ -20,17 +21,17 @@ vue_component = {
                 temp.style = `${file.split(/<style>|<\/style>/gm)[1]}`
                 // temp.head = file.split(/<head>|<\/head>/gm)[1]
                 temp.html = get_html(file,temp)
-                let component = `Vue.component("${obj.name}",
+                let component = `Vue.component("${obj.prefix}",
                 {template:\`${temp.html.replace(/\`/gm,"\\\`")}\`,
                 ${temp.script.split(/{/gm).splice(1).join("{")})
                     let $$styleSheet = document.createElement("style")
-                        $$styleSheet.innerText = \`${temp.style.replace(/\$\$/g, obj.name.replace(".",""))}\`
+                        $$styleSheet.innerText = \`${temp.style.replace(/\$\$/g, obj.prefix)}\`
                         document.head.appendChild($$styleSheet)
                 `
 
                     
                 // fs.writeFile("test.js",component,err=>{
-                    cb(null,component.replace(/\$\$/g, obj.name))
+                    cb(null,component.replace(/\$\$/g, obj.prefix))
                 // })
             }
         })
